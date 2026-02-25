a0c712a79a51139476f74e09d29f910cfb34bd25
Carlos <carloseduardo33954580@gmail.com>
2026-02-25 19:01:24 -0300

    Add files via upload

---
+++ IA.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from openai import OpenAI
+
+modelo = OpenAI(api_key="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")
+
+st.write("### Capivara Maluca 🦫") # markdown
+
+#lista_mensagem as lm
+
+if not "lm" in st.session_state:
+    st.session_state["lm"] = []
+
+for mensagem in st.session_state["lm"]:
+    role = mensagem["role"]
+    content = mensagem["content"]
+    st.chat_message(role).write(content)
+
+msg_user = st.chat_input("Escreva sua mensagem aqui")
+
+if msg_user:
+    st.chat_message("user").write(msg_user)
+    mensagem = {"role": "user", "content": msg_user}
+    st.session_state["lm"].append(mensagem)
+
+    
