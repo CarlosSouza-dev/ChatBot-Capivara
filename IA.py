aac694e9d9eab7c68984cc1430b5facac94ca11c
Carlos <carloseduardo33954580@gmail.com>
2026-03-02 12:03:03 -0300

    teste completo

--- IA.py
+++ IA.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from openai import OpenAI
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
 modelo = OpenAI(api_key="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")
 
 st.write("### Capivara Maluca 🦫") # markdown
@@ -21,5 +21,3 @@ if msg_user:
     st.chat_message("user").write(msg_user)
     mensagem = {"role": "user", "content": msg_user}
     st.session_state["lm"].append(mensagem)
-
-    bdghrtehgrgertgher
