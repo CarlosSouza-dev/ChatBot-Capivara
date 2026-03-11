03eafdb284108dd3ca64e3fbee550b254a20ed2c
Carlos <carloseduardo33954580@gmail.com>
2026-03-11 11:52:53 -0300

    versão final e corrigida

--- IA.py
+++ IA.py
@@ -18,6 +18,19 @@ for mensagem in st.session_state["lm"]:
 msg_user = st.chat_input("Escreva sua mensagem aqui")
 
 if msg_user:
+    #msg ususario
     st.chat_message("user").write(msg_user)
     mensagem = {"role": "user", "content": msg_user}
     st.session_state["lm"].append(mensagem)
+
+    #resposta ia
+    #resposta do modelo as rm
+
+    rm = modelo.chat.completions.create(messages=st.session_state["lm"], model="gpt-4o")
+
+    resp_ia = rm.choices[0].message.content
+    
+    # exibir a resposta da IA
+    st.chat_message("assistant", avatar="🦫").write(resp_ia)
+    msg_ia = {"role": "assistant", "content": resp_ia}
+    st.session_state["lm"].append(msg_ia)
